9f3467bc3da8358c729153e3ee97d2779258fb03
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-07-14 23:55:36 +0200

    Fish: Update config, hide the welcome message and setup environment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,16 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+## Set values
+# Hide welcome message
+set fish_greeting
+
+## Environment setup
+# Apply .profile: use this to put fish compatible .profile stuff in
+if test -f ~/.fish_profile
+  source ~/.fish_profile
+end
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
 end
