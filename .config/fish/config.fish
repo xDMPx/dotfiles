67ca414ab62c1b0294cff2fef86704c0e428558f
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-07-16 23:40:37 +0200

    Fish: Replace `ls` with `exa`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,3 +19,13 @@ end
 if status --is-interactive
    source ("/usr/bin/starship" init fish --print-full-init | psub)
 end
+
+## Useful aliases
+# Replace ls with exa
+alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
+alias ip="ip -color"
+
