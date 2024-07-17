893c433dc498e0e5be40b3bc29d5a8a983822df7
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-07-17 18:30:16 +0200

    Neovim: Add an initial configuration

---
+++ .config/nvim/init.lua
@@ -0,0 +1,18 @@
+-- normalize tabulation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Hybrid line numbering
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- enable highlight groups (enable 24-bit colour)
+vim.opt.termguicolors = true
+
+-- auto-format file on wirte
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+-- yank to system clipboard
+vim.cmd [[set clipboard+=unnamedplus]]
