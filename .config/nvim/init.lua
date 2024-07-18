9e398ea24bba107406bf7903f4559c3327088c08
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-07-18 06:20:46 +0200

    Neovim: Adjust the `Neogit` highlighting color scheme. Transparent background and Dracula colors.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -70,5 +70,23 @@ require('nvim-treesitter.configs').setup {
 }
 
 -- git interface 
-require('neogit').setup{}
-
+require('neogit').setup{
+    -- Hides the hints at the top of the status buffer
+    disable_hint = true,
+    -- Disables changing the buffer highlights based on where the cursor is.
+    disable_context_highlighting = true,
+    -- Disables signs for sections/items/hunks
+    disable_signs = true,
+}
+-- transparent bg and dracula colors for Neogit
+vim.cmd [[hi NeogitDiffAdd guifg=#50FA7B guibg=#00000000]]
+vim.cmd [[hi NeogitDiffHeader cterm=bold gui=bold guifg=#8BE9FD guibg=#00000000]]
+vim.cmd [[hi NeogitDiffDelete guifg=#d15a5a guibg=#00000000]]
+vim.cmd [[hi NeogitHunkHeader cterm=bold gui=bold guifg=#8BE9FD guibg=#00000000]]
+vim.cmd [[hi NeogitDiffContextHighlight guibg=#00000000]]
+vim.cmd [[hi NeogitDiffContext guibg=#00000000]]
+vim.cmd [[hi NeogitCursorLine guibg=#00000000]]
+vim.cmd [[hi NeogitDiffAddHighlight guifg=#50FA7B guibg=#00000000]]
+vim.cmd [[hi NeogitDiffContextHighlight guibg=#00000000]]
+vim.cmd [[hi NeogitDiffDeleteHighlight guifg=#ff6e6e guibg=#00000000]]
+vim.cmd [[hi NeogitHunkHeaderHighlight gui=bold guifg=#8BE9FD guibg=#00000000]]
