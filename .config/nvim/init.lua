4940eb94ee5c5ec4dd6120faf5118371f7a63e1c
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-07-21 18:19:04 +0200

    Neovim: `telescope` enable search in hidden files and directories

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,6 +29,27 @@ require('dracula').setup {
 vim.cmd [[colorscheme dracula]]
 
 -- fuzzy finder
+local telescopeConfig = require('telescope.config')
+
+-- search in hidden files and directories
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+table.insert(vimgrep_arguments, '--hidden')
+table.insert(vimgrep_arguments, '--glob')
+table.insert(vimgrep_arguments, '!**/.git/*')
+
+require('telescope').setup {
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+		},
+	},
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -198,7 +219,7 @@ vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
     }
 )
 
-vim.diagnostic.config{
+vim.diagnostic.config {
     float = { 
         border = _border 
     }
