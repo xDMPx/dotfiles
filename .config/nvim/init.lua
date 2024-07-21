9ea3d349f314359ea4408c0caa6b6237ef403cb2
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-07-21 16:33:03 +0200

    Neovim: Make windows for LSP, etc. transparent

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -208,6 +208,25 @@ require('lspconfig.ui.windows').default_options = {
   border = _border
 }
 
+-- Transparent windows LSP, etc.
+-- https://vi.stackexchange.com/questions/38038/how-to-change-background-color-of-the-text-of-hover-window
+local set_hl_for_floating_window = function()
+    vim.api.nvim_set_hl(0, 'NormalFloat', {
+        link = 'Normal',
+    })
+    vim.api.nvim_set_hl(0, 'FloatBorder', {
+        bg = 'none',
+    })
+end
+
+set_hl_for_floating_window()
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+    pattern = '*',
+    desc = 'Avoid overwritten by loading color schemes later',
+    callback = set_hl_for_floating_window,
+})
+
 -- git decorations, integration for buffers
 require('gitsigns').setup {}
 
