cbbca9850521c7557df6fdd882810f3aebd5a2f7
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-10-05 20:10:43 +0200

    Neovim: Moved `TreeSitter` configuration to a separate module. Add setup function for specifying parser.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,38 +60,9 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 require('ibl').setup {}
 
 -- TreeSitter - highlighting
-require('nvim-treesitter.configs').setup {
-    -- A list of parser names, or 'all'
+require('treesitter').setup {
     ensure_installed = { 'rust', 'python', 'cpp', 'lua', 'vim', 'vimdoc', 'c_sharp', 'hlsl', 'kotlin' },
-    -- List of parsers to ignore installing (or 'all')
-    -- ignore_install = { 'javascript' },
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = false,
-    -- Indentation based on treesitter for the = operator
-    indent = { enable = true },
-
-    highlight = {
-        enable = true,
-
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        -- disable = { 'c', 'rust' },
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
 }
-vim.filetype.add({ extension = { hlsl = 'hlsl' } })
-
-
 -- package manager for LSP servers, DAP servers, linters, and formatters
 -- jdk17-openjdk required for kotlin_language_server to work with Android
 require('mason').setup {}
