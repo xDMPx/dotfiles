94c3be021ec3358380943aa1b2cd67e1cf690068
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-03-31 19:44:29 +0200

    Neovim: Fix no border around LSP hover in Neovim v0.11.0.

--- .config/nvim/lua/lsp-conf.lua
+++ .config/nvim/lua/lsp-conf.lua
@@ -66,7 +66,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local opts = { buffer = ev.buf }
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'K', function()
+            vim.lsp.buf.hover({
+                border = 'rounded',
+            })
+        end, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
         vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
